c8fda9121b986475b53f72ad7abb4837440e36d8
Alex <fogialex01@gmail.com>
2023-05-17 13:33:16 +0200

    Initial commit

---
+++ src/main/java/Beadando/progkor/autopartscatalog/AutopartsCatalogApplication.java
@@ -0,0 +1,13 @@
+package Beadando.progkor.autopartscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopartsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopartsCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Beadando/progkor/autopartscatalog/AutopartsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package Beadando.progkor.autopartscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopartsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
